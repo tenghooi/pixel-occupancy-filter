dcddcdccc91e9a86877d2241a136e2df7fcb93b8
TengHooi <chantenghooi@gmail.com>
2024-02-03 21:37:38 +0800

    some changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>  
-#include <opencv2/opencv.hpp>                 
+#include <vector>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
                    
-int main()
+int main(int argc, char** argv)
 {           
-    std::cout << "Hello world" << std::endl;
+    std::string image_path {cv::samples::findFile("../test_images/g.png")};
+    cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
+
+    if (img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path <<std::endl;
+        return 1;
+    }
+
+    cv::imshow("Display Window", img);
+    int k = cv::waitKey(0);
 
     return 0;
 }
+
